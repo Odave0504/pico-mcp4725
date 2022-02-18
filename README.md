3a193e4acee380c4d4b9c611394194b226dc2125
Odave <david.oh@odave.me>
2022-02-18 23:39:39 +0900

    add connection image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pico-mcp4725
 Simple code for control MCP4725 12bit DAC with Raspberry Pi pico
+
+![circuit](https://user-images.githubusercontent.com/99967544/154702400-487d8e09-141f-43f9-a4f5-e95b0f6539b7.png)
--- single_output.py
+++ single_output.py
@@ -1,4 +1,6 @@
-#create a I2C bus
+import machine
+
+# I2C bus settings
 i2c_channel = 1
 scl_pin = 19
 sda_pin = 18
@@ -9,16 +11,21 @@ class mcp4725:
         self.channel = channel
         self.address = address
     
-    def mcp4725_setup(self, scl_pin, sda_pin):
-        import machine
+    def mcp4725_init(self, scl_pin, sda_pin):
         self.i2c = machine.I2C(self.channel, scl=machine.Pin(scl_pin), sda=machine.Pin(sda_pin))
 
     def mcp4725_send(self, value):
-        import machine
         buf=bytearray(2)
         buf[0]=(value >> 8) & 0xFF
         buf[1]=value & 0xFF
         self.i2c.writeto(self.address, buf)
 
-test1 = mcp4725(i2c_channel, address)
-test1.mcp4725_setup(scl_pin, sda_pin)
+
+# Output value
+voltage0_out = 1.65 # 0~3.3V
+dac0_value = int(voltage0_out / 3.3 * 4095) # convert voltage to 12-bit DAC value
+
+# DAC initialization & send value
+dac0 = mcp4725(i2c_channel, address)
+dac0.mcp4725_init(scl_pin, sda_pin)
+dac0.mcp4725_send(dac0_value)
